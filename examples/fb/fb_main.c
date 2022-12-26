d41b518010d0fa2d04987eceee6260fb19847cef
Lee Lup Yuen <luppy@appkaki.com>
2022-12-26 21:42:32 +0800

    Testing TCON0_Tri_Finish_Int_Flag

--- examples/fb/fb_main.c
+++ examples/fb/fb_main.c
@@ -37,6 +37,7 @@
 #include <nuttx/video/rgbcolors.h>
 
 #include "../arch/arm64/src/common/arm64_internal.h" //// TODO
+static bool display_frame_done(void); ////
 
 /****************************************************************************
  * Preprocessor Definitions
@@ -107,6 +108,10 @@ static void draw_rect32(FAR struct fb_state_s *state,
       dest = ((FAR uint32_t *)row) + area->x;
       for (x = 0; x < area->w; x++)
         {
+      //// Begin
+      if (!display_frame_done()) {}
+      //// End
+
           *dest++ = g_rgb24[color];
 
           /* Fixes missing rows in the rendered image, not sure why */
@@ -134,6 +139,10 @@ static void draw_rect16(FAR struct fb_state_s *state,
       dest = ((FAR uint16_t *)row) + area->x;
       for (x = 0; x < area->w; x++)
         {
+      //// Begin
+      if (!display_frame_done()) {}
+      //// End
+
           *dest++ = g_rgb16[color];
 
           /* Fixes missing rows in the rendered image, not sure why */
@@ -161,6 +170,10 @@ static void draw_rect8(FAR struct fb_state_s *state,
       dest = row + area->x;
       for (x = 0; x < area->w; x++)
         {
+      //// Begin
+      if (!display_frame_done()) {}
+      //// End
+
           *dest++ = g_rgb8[color];
 
           /* Fixes missing rows in the rendered image, not sure why */
@@ -222,6 +235,10 @@ static void draw_rect1(FAR struct fb_state_s *state,
 
   for (y = 0; y < area->h; y++)
     {
+      //// Begin
+      if (!display_frame_done()) {}
+      //// End
+
       /* 'pixel' points to the 1st pixel the next row */
 
       /* Special case: The row starts and ends within the same byte */
@@ -498,4 +515,73 @@ int main(int argc, FAR char *argv[])
   munmap(state.fbmem, state.pinfo.fblen);
   close(state.fd);
   return EXIT_SUCCESS;
+}
+
+#include "../arch/arm64/src/common/arm64_arch.h"
+
+#define SUNXI_LCD0_BASE			0x01c0C000
+#define u32 uint32_t
+#define u8 uint8_t
+#define readl(addr) getreg32((unsigned long) (addr))
+#define writel(data, addr) putreg32(data, (unsigned long) (addr))
+
+struct sunxi_lcdc_reg {
+	u32 ctrl;			/* 0x00 */
+	u32 int0;			/* 0x04 */
+	u32 int1;			/* 0x08 */
+	u8 res0[0x04];			/* 0x0c */
+	u32 tcon0_frm_ctrl;		/* 0x10 */
+	u32 tcon0_frm_seed[6];		/* 0x14 */
+	u32 tcon0_frm_table[4];		/* 0x2c */
+	u8 res1[4];			/* 0x3c */
+	u32 tcon0_ctrl;			/* 0x40 */
+	u32 tcon0_dclk;			/* 0x44 */
+	u32 tcon0_timing_active;	/* 0x48 */
+	u32 tcon0_timing_h;		/* 0x4c */
+	u32 tcon0_timing_v;		/* 0x50 */
+	u32 tcon0_timing_sync;		/* 0x54 */
+	u32 tcon0_hv_intf;		/* 0x58 */
+	u8 res2[0x04];			/* 0x5c */
+	u32 tcon0_cpu_intf;		/* 0x60 */
+	u32 tcon0_cpu_wr_dat;		/* 0x64 */
+	u32 tcon0_cpu_rd_dat0;		/* 0x68 */
+	u32 tcon0_cpu_rd_dat1;		/* 0x6c */
+	u32 tcon0_ttl_timing0;		/* 0x70 */
+	u32 tcon0_ttl_timing1;		/* 0x74 */
+	u32 tcon0_ttl_timing2;		/* 0x78 */
+	u32 tcon0_ttl_timing3;		/* 0x7c */
+	u32 tcon0_ttl_timing4;		/* 0x80 */
+	u32 tcon0_lvds_intf;		/* 0x84 */
+	u32 tcon0_io_polarity;		/* 0x88 */
+	u32 tcon0_io_tristate;		/* 0x8c */
+	u32 tcon1_ctrl;			/* 0x90 */
+	u32 tcon1_timing_source;	/* 0x94 */
+	u32 tcon1_timing_scale;		/* 0x98 */
+	u32 tcon1_timing_out;		/* 0x9c */
+	u32 tcon1_timing_h;		/* 0xa0 */
+	u32 tcon1_timing_v;		/* 0xa4 */
+	u32 tcon1_timing_sync;		/* 0xa8 */
+	u8 res3[0x44];			/* 0xac */
+	u32 tcon1_io_polarity;		/* 0xf0 */
+	u32 tcon1_io_tristate;		/* 0xf4 */
+	u32 ecc_fifo;			/* 0xf8 */
+	u8 res4[0x104];			/* 0xfc */
+	u32 mux_ctrl;			/* 0x200 */
+	u8 res5[0x1c];			/* 0x204 */
+	u32 lvds_ana0;			/* 0x220 */
+	u32 lvds_ana1;			/* 0x224 */
+};
+
+// Bit 11 is TCON0_Tri_Finish_Int_Flag
+// Trigger when cpu trigger mode finish
+static bool display_frame_done(void)
+{
+	struct sunxi_lcdc_reg * const lcdc = (struct sunxi_lcdc_reg *)SUNXI_LCD0_BASE;
+
+	uint32_t status = readl(&lcdc->int0);
+	if (status & BIT(11)) {
+		writel(0, &lcdc->int0); /* Disable all interrupts */
+	}
+
+	return status & BIT(11);
 }
