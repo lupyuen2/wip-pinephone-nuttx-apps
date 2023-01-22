4cdc3eb2da52a540930a9b0a0ab0ae6d1d0713ae
Lee Lup Yuen <luppy@appkaki.com>
2023-01-22 20:39:33 +0800

    Clean up

--- examples/lvgldemo/lvgldemo.c
+++ examples/lvgldemo/lvgldemo.c
@@ -246,228 +246,12 @@ int main(int argc, FAR char *argv[])
 
 //////////////////////////////////////////////////////////////////////////
 // Testing LVGL Terminal
-#include "nshlib/nshlib.h"
-
-////#define TEST_PTY 
-#ifdef TEST_PTY
-//////////////////////////////////////////////////////////////////////////
-// Pseudo Terminal Implementation
-
-#ifndef CONFIG_PSEUDOTERM
-#error Please enable "Device Drivers > Serial Driver Support > Pseudo-Terminal (PTY) support"
-#endif
-
-#include <termios.h>
-#include <poll.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <sys/ioctl.h>
-#include <nuttx/serial/pty.h>
-
-struct term_pair_s
-{
-  ////int fd_uart;
-  int fd_pty;
-};
-
-void test_terminal(void)
-{
-  _info("test_terminal\n");
-  struct term_pair_s termpair;
-  /////struct termios tio;
-  pthread_t si_thread;
-  pthread_t so_thread;
-  char buffer[16];
-  pid_t pid;
-  int fd_pts;
-  int ret;
-
-  printf("Create pseudo-terminal\n");
-
-  /* Open pseudo-terminal master (pts factory) */
-
-  termpair.fd_pty = open("/dev/ptmx", O_RDWR | O_NOCTTY);
-  if (termpair.fd_pty < 0)
-    {
-      fprintf(stderr, "ERROR: Failed to opening /dev/ptmx: %d\n",
-              errno);
-      return;
-    }
-
-  /* Grant access and unlock the slave PTY */
-
-  ret = grantpt(termpair.fd_pty);
-  if (ret < 0)
-    {
-      fprintf(stderr, "ERROR: grantpt() failed: %d\n", errno);
-      goto error_pts;
-    }
-
-  ret = unlockpt(termpair.fd_pty);
-  if (ret < 0)
-    {
-      fprintf(stderr, "ERROR: unlockpt() failed: %d\n", errno);
-      goto error_pts;
-    }
-
-  /* Get the create pts file-name */
-
-  ret = ptsname_r(termpair.fd_pty, buffer, 16);
-  if (ret < 0)
-    {
-      fprintf(stderr, "ERROR: ptsname_r() failed: %d\n", errno);
-      goto error_pts;
-    }
-
-  /* Open the created pts */
-
-  fd_pts = open(buffer, O_RDWR);
-  if (fd_pts < 0)
-    {
-      fprintf(stderr, "ERROR: Failed to open %s: %d\n", buffer, errno);
-      goto error_pts;
-    }
-
-#ifdef NOTUSED
-  /* Open the second serial port to create a new console there */
-
-  termpair.fd_uart = open(CONFIG_EXAMPLES_PTYTEST_SERIALDEV, O_RDWR);
-  if (termpair.fd_uart < 0)
-    {
-#ifdef CONFIG_EXAMPLES_PTYTEST_WAIT_CONNECTED
-      /* if ENOTCONN is received, re-attempt to open periodically */
-
-      if (errno == ENOTCONN)
-        {
-          fprintf(stderr, "ERROR: device not connected, will continue"
-                          " trying\n");
-        }
-
-      while (termpair.fd_uart < 0 && errno == ENOTCONN)
-        {
-          sleep(1);
-
-          termpair.fd_uart = open(CONFIG_EXAMPLES_PTYTEST_SERIALDEV, O_RDWR);
-        }
-
-      /* if we exited due to an error different than ENOTCONN */
-
-      if (termpair.fd_uart < 0)
-        {
-          fprintf(stderr, "Failed to open %s: %i\n",
-                 CONFIG_EXAMPLES_PTYTEST_SERIALDEV, errno);
-          goto error_serial;
-        }
-#else
-      fprintf(stderr, "Failed to open %s: %i\n",
-             CONFIG_EXAMPLES_PTYTEST_SERIALDEV, errno);
-      goto error_serial;
-#endif
-    }
-
-#ifdef CONFIG_SERIAL_TERMIOS
-  /* Enable \n -> \r\n conversion during write */
-
-  ret = tcgetattr(termpair.fd_uart, &tio);
-  if (ret)
-    {
-      fprintf(stderr, "ERROR: tcgetattr() failed: %d\n", errno);
-      goto error_serial;
-    }
-
-  tio.c_oflag = OPOST | ONLCR;
-  ret = tcsetattr(termpair.fd_uart, TCSANOW, &tio);
-  if (ret)
-    {
-      fprintf(stderr, "ERROR: tcsetattr() failed: %d\n", errno);
-      goto error_serial;
-    }
-#endif
-#endif  // NOTUSED
-
-  printf("Starting a new NSH Session using %s\n", buffer);
-
-  /* Close default stdin, stdout and stderr */
-
-  close(0);
-  close(1);
-  close(2);
-
-  /* Use this pts file as stdin, stdout, and stderr */
-
-  dup2(fd_pts, 0);
-  dup2(fd_pts, 1);
-  dup2(fd_pts, 2);
-
-  /* Create a new console using this /dev/pts/N */
-
-  char *argv[] = { NULL };
-  pid = task_create(
-    "NSH Console", 
-    100,  // Priority
-    CONFIG_DEFAULT_TASK_STACKSIZE,
-    nsh_consolemain,
-    argv
-  );
-  _info("task_create: pid=%d\n", pid);
-  if (pid < 0)
-    {
-      _err("task_create failed: %d\n", pid);
-      goto error_console;
-    }
-
-  /* Start a thread to read from serial */
-
-  ret = pthread_create(&si_thread, NULL,
-                       (pthread_startroutine_t)serial_in,
-                       (pthread_addr_t)&termpair);
-  _info("pthread_create serial_in: %d\n", ret);
-  if (ret != 0)
-    {
-      _err("pthread_create serial_in failed: %d\n", pid);
-      goto error_thread1;
-    }
-
-  /* Start a thread to write to serial */
-
-  ret = pthread_create(&so_thread, NULL,
-                       (pthread_startroutine_t)serial_out,
-                       (pthread_addr_t)&termpair);
-  _info("pthread_create serial_out: %d\n", ret);
-  if (ret != 0)
-    {
-      _err("pthread_create serial_out failed: %d\n", pid);
-      goto error_thread2;
-    }
-
-  /* Stay here to keep the threads running */
-
-  while (true)
-    {
-      /* Nothing to do, then sleep to avoid eating all cpu time */
-
-      pause();
-    }
-
-  return;
-
-error_thread2:
-error_thread1:
-error_console:
-  ////close(termpair.fd_uart);
-error_serial:
-  close(fd_pts);
-error_pts:
-  close(termpair.fd_pty);
-}
-
-#else  // !TEST_PTY
-//////////////////////////////////////////////////////////////////////////
-// Pipe Implementation
 
 #ifndef CONFIG_DEV_PIPE_SIZE
 #error Please enable "Device Drivers > FIFO and named pipe drivers" in menuconfig
 #endif
+
+#include "nshlib/nshlib.h"
 
 void test_terminal(void)
 {
@@ -507,16 +291,13 @@ void test_terminal(void)
     nsh_consolemain,
     argv
   );
-  if (pid < 0)
-    {
-      _err("task_create failed: %d\n", errno);
-      return;
-    }
+  if (pid < 0) { _err("task_create failed: %d\n", errno); return; }
   _info("pid=%d\n", pid);
 
   // Wait a while
   sleep(1);
 
+  // Send a few commands to NSH
   for (int i = 0; i < 5; i++) {
 
     // Send a command to NSH stdin
@@ -531,7 +312,8 @@ void test_terminal(void)
     // Wait a while
     sleep(1);
 
-    // Read the output from NSH stdout
+    // Read the output from NSH stdout.
+    // TODO: This will block if there's nothing to read.
     static char buf[64];
     ret = read(
       nsh_stdout[READ_PIPE],
@@ -548,7 +330,8 @@ void test_terminal(void)
     sleep(1);
 
 #ifdef NOTUSED
-    // Read the output from NSH stderr
+    // Read the output from NSH stderr.
+    // TODO: This will block if there's nothing to read.
     ret = read(    
       nsh_stderr[READ_PIPE],
       buf,
@@ -615,4 +398,3 @@ nsh>
 nsh> 
 n
 */
-#endif  // !TEST_PTY
