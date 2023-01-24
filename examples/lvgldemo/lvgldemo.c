1b22f810fe0187a78a18871699853fa039c6c7df
Lee Lup Yuen <luppy@appkaki.com>
2023-01-24 19:37:49 +0800

    Adding keyboard

--- examples/lvgldemo/lvgldemo.c
+++ examples/lvgldemo/lvgldemo.c
@@ -225,7 +225,7 @@ int main(int argc, FAR char *argv[])
 
   /* LVGL demo creation */
 
-  demo_create_func();
+  ////demo_create_func();
 
   /* Handle LVGL tasks */
 
@@ -255,6 +255,8 @@ int main(int argc, FAR char *argv[])
 
 static bool has_input(int fd);
 static void my_timer(lv_timer_t * timer);
+static void lv_example_keyboard_1(void);
+static void ta_event_cb(lv_event_t * e);
 
 // Pipes for NSH Shell: : stdin, stdout, stderr
 static int nsh_stdin[2];
@@ -304,6 +306,9 @@ void test_terminal(void) {
     &user_data  // Callback Data
   );
   UNUSED(timer);
+
+  // Create the LVGL Terminal Widgets
+  lv_example_keyboard_1();
 }
 
 // Callback for LVGL Timer
@@ -396,9 +401,42 @@ static bool has_input(int fd) {
   return false;
 }
 
-// TODO: Read input from LVGL Text Area Widget
+static void lv_example_keyboard_1(void)
+{
+    /*Create a keyboard to use it with an of the text areas*/
+    lv_obj_t * kb = lv_keyboard_create(lv_scr_act());
 
-// TODO: Send input to NSH Stdin
+    /*Create a text area. The keyboard will write here*/
+    lv_obj_t * ta;
+    ta = lv_textarea_create(lv_scr_act());
+    lv_obj_align(ta, LV_ALIGN_TOP_LEFT, 10, 10);
+    lv_obj_add_event_cb(ta, ta_event_cb, LV_EVENT_ALL, kb);
+    lv_textarea_set_placeholder_text(ta, "Hello");
+    lv_obj_set_size(ta, 140, 80);
+
+    ta = lv_textarea_create(lv_scr_act());
+    lv_obj_align(ta, LV_ALIGN_TOP_RIGHT, -10, 10);
+    lv_obj_add_event_cb(ta, ta_event_cb, LV_EVENT_ALL, kb);
+    lv_obj_set_size(ta, 140, 80);
+
+    lv_keyboard_set_textarea(kb, ta);
+}
+
+static void ta_event_cb(lv_event_t * e)
+{
+    lv_event_code_t code = lv_event_get_code(e);
+    lv_obj_t * ta = lv_event_get_target(e);
+    lv_obj_t * kb = lv_event_get_user_data(e);
+    if(code == LV_EVENT_FOCUSED) {
+        lv_keyboard_set_textarea(kb, ta);
+        lv_obj_clear_flag(kb, LV_OBJ_FLAG_HIDDEN);
+    }
+
+    if(code == LV_EVENT_DEFOCUSED) {
+        lv_keyboard_set_textarea(kb, NULL);
+        lv_obj_add_flag(kb, LV_OBJ_FLAG_HIDDEN);
+    }
+}
 
 /* Output:
 DRAM: 2048 MiB
